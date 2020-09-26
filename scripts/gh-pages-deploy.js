808a630282f5124a56c28b62398a5e5af161a7e7
Alfonso Pacheco <alcheco@gmail.com>
2020-09-26 01:46:55 -0300

    FIX: branch name ⚡️

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -3,13 +3,13 @@ const fs = require("fs");
 
 (async () => {
   try {
-    await execa("git", ["checkout", "--orphan", "Deploy 🥂"]);
+    await execa("git", ["checkout", "--orphan", "gh-pages"]);
     console.log("Building...");
     await execa("npm", ["run", "build"]);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync("dist") ? "dist" : "build";
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
-    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+    await execa("git", ["--work-tree", folderName, "commit", "-m", "Deploy 🥂"]);
     console.log("Pushing to gh-pages...");
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
     await execa("rm", ["-r", folderName]);
